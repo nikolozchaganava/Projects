060ceca7ba33a11d404dcffe1c9abf226f1ca4de
nikolozchaganava <nikolozchaganava333@gmail.com>
2025-02-25 21:09:35 +0400

    second commit

--- Goa Website/Goa-Project/src/main.jsx
+++ Goa Website/Goa-Project/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Home />
     </BrowserRouter>
   </StrictMode>
 );
